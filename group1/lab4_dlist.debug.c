5a544e5b1c4e236a313e340dce46ab006f9f07a9
tc-imba <liuyh615@sjtu.edu.cn>
2017-12-08 12:25:24 +0800

    g1 diff

--- group1/lab4_dlist.debug.c
+++ group1/lab4_dlist.debug.c
@@ -4,27 +4,27 @@
 #include <stdlib.h>
 
 struct node {
-    char *key;
+    char* key;
     dlistValue value;
-    struct node *next;
-    struct node *prev;
+    struct node* next;
+    struct node* prev;
 };
 
 struct linkedList {
     int size;
-    struct node *head;
-    struct node *tail;
+    struct node* head;
+    struct node* tail;
     dlistValueType type;
 };
 
-int compLarge(const struct node *node1, const struct node *node2, dlistValueType type);
-int compSmall(const struct node *node1, const struct node *node2, dlistValueType type);
+int compLarge(const struct node* node1, const struct node* node2, dlistValueType type);
+int compSmall(const struct node* node1, const struct node* node2, dlistValueType type);
 
 dlist createDlist(dlistValueType type) {
     // EFFECTS: allocate and create a new dlist of datatype ' type ' object
     // returns the created dlist object on success
     // returns NULL on error
-    struct linkedList *list = (struct linkedList *) malloc(sizeof(struct linkedList) * MAXLINENUM);
+    struct linkedList* list = (struct linkedList*)malloc(sizeof(struct linkedList)*MAXLINENUM);
     list->size = 0;
     list->head = list->tail = NULL;
     list->type = type;
@@ -35,24 +35,25 @@ dlist createDlist(dlistValueType type) {
 int dlistIsEmpty(dlist_const this) {
     // REQUIRES: argument ' this ' is non-null and is a valid list
     // EFFECTS: return whether the list ' this ' is empty
-    struct linkedList *list = (struct linkedList *) this;
+    struct linkedList* list = (struct linkedList*)this;
     return (list->size == 0);
 }
 
-void dlistAppend(dlist this, const char *key, dlistValue value) {
+void dlistAppend(dlist this, const char* key, dlistValue value) {
     // REQUIRES: type instantiated in ' value ' corresponds to the datatype in the list
     //
-    // EFFECTS : add a line of form ”key=value�?to the tail of the dlist
+    // EFFECTS : add a line of form ”key=value�?to the tail of the dlist
     // MODIFIES: modifies ' this ' .
-    struct linkedList *list = (struct linkedList *) this;
-    struct node *tnode = (struct node *) malloc(sizeof(struct node));
+    struct linkedList* list = (struct linkedList*)this;
+    struct node* tnode = (struct node*)malloc(sizeof(struct node));
     tnode->key = key;
     tnode->value = value;
     if (dlistIsEmpty(list)) {
         list->head = list->tail = tnode;
         tnode->prev = NULL;
         tnode->next = NULL;
-    } else {
+    }
+    else {
         list->tail->next = tnode;
         tnode->prev = list->tail;
         tnode->next = NULL;
@@ -72,61 +73,57 @@ void dlistSort(dlist_const this, dlist listDst, dlistSortMethod method) {
     // * Note ' listDst ' may be non-empty when invoked.
     // * Be very careful with memory management regarding string lists.
     // MODIFIES: argument ' listDst 'argument ' listDst ' is not null and is a valid list
-    struct linkedList *list = (struct linkedList *) this;
-    struct node *tnode = list->head;
+    struct linkedList* list = (struct linkedList*)this;
+    struct node* tnode = list->head;
     if (NULL == tnode) {
         printf("the sorted list is empty!\n");
         return;
     }
-    struct linkedList *listdst = (struct linkedList *) listDst;
+    struct linkedList* listdst = (struct linkedList*)listDst;
     dlistValueType type = list->type;
     // if 'this' is empty, leave listDst unchanged
     // else free nodes in listDst
     for (int i = 0; i < listdst->size; i++) {
-        struct node *temp = listdst->head;
+        struct node* temp = listdst->head;
         listdst->head = temp->next;
         free(temp);
     }
     listdst->size = 0;
     // sort
-    while (NULL != tnode) {
-        struct node *addednode = (struct node *) malloc(sizeof(struct node));
-
-        //addednode->key = tnode->key; @bug
+    while(NULL != tnode) {
+        struct node* addednode = (struct node*)malloc(sizeof(struct node));
         addednode->key = malloc(strlen(tnode->key) + 1);
         strcpy(addednode->key, tnode->key);
-
         addednode->value = tnode->value;
         if (list->type == DLIST_STR) {
             addednode->value.dlistValue = malloc(strlen(tnode->value.dlistValue) + 1);
             strcpy(addednode->value.dlistValue, tnode->value.dlistValue);
         }
-
-
-        if (dlistIsEmpty(listdst)) {
+        if(dlistIsEmpty(listdst)) {
             addednode->prev = NULL;
             addednode->next = NULL;
             listdst->head = listdst->tail = addednode;
-        } else {
-            struct node *t = listdst->head;
+        }
+        else {
+            struct node* t = listdst->head;
             int flag;
-            while (NULL != t) {
+            while(NULL != t) {
                 flag = 0;
-                switch (method) {
-                case 2: { // inc
-                    if (compLarge(addednode, t, type)) {
-                        t = t->next;
-                        flag = 1;
+                switch(method) {
+                    case 2: { // inc
+                        if (compLarge(addednode, t, type)) {
+                            t = t->next;
+                            flag = 1;
+                        }
+                        break;
                     }
-                    break;
-                }
-                case 3: { // dec
-                    if (compSmall(addednode, t, type)) {
-                        t = t->next;
-                        flag = 1;
+                    case 3: { // dec
+                        if (compSmall(addednode, t, type)) {
+                            t = t->next;
+                            flag = 1;
+                        }
+                        break;
                     }
-                    break;
-                }
                 }
                 if (flag) continue;
                 flag = 0;
@@ -136,7 +133,8 @@ void dlistSort(dlist_const this, dlist listDst, dlistSortMethod method) {
                     addednode->prev = NULL;
                     addednode->next = t;
                     listdst->head = addednode;
-                } else {
+                }
+                else {
                     t->prev->next = addednode;
                     addednode->prev = t->prev;
                     addednode->next = t;
@@ -161,22 +159,23 @@ void dlistPrint(dlist_const this) {
     // REQUIRES: argument ' this ' is not null and is a valid list
     // EFFECTS: Print the content of ' this ' in the required format to standard output.
     // A newline must be followed after each line (including the last one).
-    struct linkedList *list = (struct linkedList *) this;
+    struct linkedList* list = (struct linkedList*)this;
     dlistValueType type = list->type;
     if (dlistIsEmpty(list)) {
         printf("The list is empty!\n");
-    } else {
-        struct node *tnode = list->head;
-        while (NULL != tnode) {
+    }
+    else {
+        struct node* tnode = list->head;
+        while(NULL != tnode) {
             printf("%s=", tnode->key);
-            switch (type) {
-            case 1: // int
+            switch(type) {
+                case 1: // int
                 printf("%d\n", tnode->value.intValue);
                 break;
-            case 2: // string
+                case 2: // string
                 printf("%s\n", tnode->value.dlistValue);
                 break;
-            case 3: // double
+                case 3: // double
                 printf("%g\n", tnode->value.doubleValue);
                 break;
             }
@@ -190,11 +189,8 @@ void dlistFree(dlist this) {
     // EFFECTS: if ' this ' is NULL do nothing, other wise frees the list
     // By freeing the list the user also needs to free the data the list manages
     if (NULL == this) return;
-    struct linkedList *list = (struct linkedList *) this;
-    /**
-     * @bug no free if value is string
-     * But can't ensure this is a bug or they forgot it
-     */
+    struct linkedList* list = (struct linkedList*)this;
+    
     for (int i = 0; i < list->size; i++) {
         if (i == list->size - 1) {
             free(list->head->key);
@@ -202,7 +198,8 @@ void dlistFree(dlist this) {
                 free(list->head->value.dlistValue);
             }
             free(list->head);
-        } else {
+        }
+        else {
             list->head = list->head->next;
             free(list->head->prev->key);
             if (list->type == DLIST_STR) {
@@ -215,57 +212,57 @@ void dlistFree(dlist this) {
 }
 
 // if node1.value > node2.value, return 1, else return 0
-int compLarge(const struct node *node1, const struct node *node2, dlistValueType type) {
-    switch (type) {
-    case 1: { // int
-        int a = node1->value.intValue;
-        int b = node2->value.intValue;
-        if (a > b) return 1;
-        else return 0;
-    }
-    case 2: { // char*
-        char *a = node1->value.dlistValue;
-        char *b = node2->value.dlistValue;
-        if (strcmp(a, b) > 0) return 1;
-        else return 0;
-    }
-    case 3: { // double
-        double a = node1->value.doubleValue;
-        double b = node2->value.doubleValue;
-        if (a > b) return 1;
-        else return 0;
-    }
-    default: {
-        printf("unknown type!\n");
-        exit(-1);
-    }
+int compLarge(const struct node* node1, const struct node* node2, dlistValueType type) {
+    switch(type) {
+        case 1: { // int
+            int a = node1->value.intValue;
+            int b = node2->value.intValue;
+            if(a > b) return 1;
+            else return 0;
+        }
+        case 2: { // char*
+            char* a = node1->value.dlistValue;
+            char* b = node2->value.dlistValue;
+            if(strcmp(a, b) > 0) return 1;
+            else return 0;
+        }
+        case 3: { // double
+            double a = node1->value.doubleValue;
+            double b = node2->value.doubleValue;
+            if(a > b) return 1;
+            else return 0;
+        }
+        default: {
+            printf("unknown type!\n");
+            exit(-1);
+        }
     }
 }
 
 // if node1.value < node2.value, return 1, else return 0
-int compSmall(const struct node *node1, const struct node *node2, dlistValueType type) {
-    switch (type) {
-    case 1: { // int
-        int a = node1->value.intValue;
-        int b = node2->value.intValue;
-        if (a < b) return 1;
-        else return 0;
-    }
-    case 2: { // char*
-        char *a = node1->value.dlistValue;
-        char *b = node2->value.dlistValue;
-        if (strcmp(a, b) < 0) return 1;
-        else return 0;
-    }
-    case 3: { // double
-        double a = node1->value.doubleValue;
-        double b = node2->value.doubleValue;
-        if (a < b) return 1;
-        else return 0;
-    }
-    default: {
-        printf("unknown type!\n");
-        exit(-1);
-    }
+int compSmall(const struct node* node1, const struct node* node2, dlistValueType type) {
+    switch(type) {
+        case 1: { // int
+            int a = node1->value.intValue;
+            int b = node2->value.intValue;
+            if(a < b) return 1;
+            else return 0;
+        }
+        case 2: { // char*
+            char* a = node1->value.dlistValue;
+            char* b = node2->value.dlistValue;
+            if(strcmp(a, b) < 0) return 1;
+            else return 0;
+        }
+        case 3: { // double
+            double a = node1->value.doubleValue;
+            double b = node2->value.doubleValue;
+            if(a < b) return 1;
+            else return 0;
+        }
+        default: {
+            printf("unknown type!\n");
+            exit(-1);
+        }
     }
 }
--- group1/main.debug.c
+++ group1/main.debug.c
@@ -3,63 +3,65 @@
 #include <string.h>
 #include "lab4_dlist.h"
 
-int main(int argc, char *argv[]) {
-    char *filename = argv[1];
-    FILE *inputFile = fopen(filename, "r");
-    char *order = argv[2];
+int main(int argc, char* argv[]) {
+    char* filename = argv[1];
+    FILE* inputFile = fopen(filename, "r");
+    char* order = argv[2];
     dlistSortMethod sort;
-    if (!strcmp(order, "rand")) {
+    if (!strcmp(order, "rand")){
         sort = DLIST_SORT_RAND;
-    } else if (!strcmp(order, "inc")) {
+    }
+    else if (!strcmp(order, "inc")) {
         sort = DLIST_SORT_INC;
-    } else if (!strcmp(order, "dec")) {
+    }
+    else if (!strcmp(order, "dec")) {
         sort = DLIST_SORT_DEC;
     }
-    char *token;
+    char* token;
     token = strtok(filename, "_");
-    char *orderGiven = token;
-    char *typeGiven = strtok(NULL, ".");
+    char* orderGiven = token;
+    char* typeGiven = strtok(NULL, ".");
     dlistValueType type;
     if (!strcmp(typeGiven, "int")) {
         type = DLIST_INT;
-    } else if (!strcmp(typeGiven, "char*")) {
+    }
+    else if (!strcmp(typeGiven, "char*")) {
         type = DLIST_STR;
-    } else if (!strcmp(typeGiven, "double")) {
+    }
+    else if (!strcmp(typeGiven, "double")) {
         type = DLIST_DOUBLE;
     }
     dlist list = createDlist(type);;
 
-    while (!feof(inputFile)) {
+    while(!feof(inputFile)) {
         // read information and process
         char tempstr[MAXCHARINLINE];
         fgets(tempstr, MAXLINENUM, inputFile);
         token = strtok(tempstr, "=");
-        char *tempkey = (char *) malloc(sizeof(char) * MAXCHARINLINE);
+        char* tempkey = (char*)malloc(sizeof(char)*MAXCHARINLINE);
 
-        // tempkey = token; @bug
         strcpy(tempkey, token);
 
         token = strtok(NULL, "=");
         dlistValue value;
-
-        switch (type) {
-        case 1: // int
+        
+        switch(type) {
+            case 1: // int
             value.intValue = atoi(token);
             break;
-        case 2: // string
-            // value.dlistValue = token @bug
+            case 2: // string
             if (token[strlen(token) - 1] == '\n') {
                 token[strlen(token) - 1] = '\0';
             }
             value.dlistValue = malloc(strlen(token) + 1);
             strcpy(value.dlistValue, token);
             break;
-        case 3: // double
+            case 3: // double
             value.doubleValue = atof(token);
             break;
         }
         // add node
-
+        
         dlistAppend(list, tempkey, value);
     }
     dlist dst = createDlist(type);
